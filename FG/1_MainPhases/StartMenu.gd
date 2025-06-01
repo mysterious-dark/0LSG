191fc71e44a2e23091a999d2863d3190588ca46b
mysterious-dark <tuanmoc21200@gmail.com>
2025-06-01 18:30:53 +0700

    updated touch event

--- FG/1_MainPhases/StartMenu.gd
+++ FG/1_MainPhases/StartMenu.gd
@@ -8,6 +8,8 @@ extends Control
 @onready var news_feed2 = $MainContainer/HBoxContainer/LeftSide/NewsScreen2
 @onready var gacha_feed = $MainContainer/HBoxContainer/RightContainer/TopSection/NewsScreen
 
+@onready var startButton = $MainContainer/HBoxContainer/RightContainer/BottomSection/ButtonControl/StartButton
+
 func _ready():
 	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS
 	get_tree().root.content_scale_aspect = Window.CONTENT_SCALE_ASPECT_EXPAND
@@ -24,7 +26,33 @@ func _ready():
 	# Connect to window size changes
 	get_tree().root.size_changed.connect(_on_window_size_changed)
 	_on_window_size_changed()
+	# Connect the start button
+	startButton.gui_input.connect(_on_start_button_input)
 
+func _on_start_button_input(event):
+	# Handle touch screen input
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			_on_start_button_pressed()
+			
+func _on_start_button_pressed():
+	# Change to the hub scene
+	#get_tree().change_scene_to_file("res://1_MainPhases/Hub/hub.tscn")
+		# Optional: Add a fade transition
+	var transition = ColorRect.new()
+	transition.color = Color.BLACK
+	transition.modulate = Color(0, 0, 0, 0)  # Start fully transparent
+	transition.set_anchors_preset(Control.PRESET_FULL_RECT)
+	add_child(transition)
+	
+	# Create a tween for the fade effect
+	var tween = create_tween()
+	tween.tween_property(transition, "modulate", Color(0, 0, 0, 1), 0.5)  # Fade to black
+	await tween.finished
+	
+	# Change scene
+	get_tree().change_scene_to_file("res://1_MainPhases/Hub/hub.tscn")
+	
 func _on_window_size_changed():
 	var window_size = DisplayServer.window_get_size()
 	var is_portrait = window_size.y > window_size.x
